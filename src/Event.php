b71d180f6802efd56018e47ad794a7ccd27363c4
thewindear <thewindear@Outlook.com>
2023-03-01 00:46:39 +0800

    feat(Event)

--- src/Event.php
+++ src/Event.php
@@ -6,8 +6,8 @@ class Event
 {
     use SerializesModels;
 
-    public $queue = 'default';
-    public $pool = 'default';
+    //public $queue = 'default';
+    //public $pool = 'default';
     public $delay = 0;
     public $data;
     public function __construct(array $data)
